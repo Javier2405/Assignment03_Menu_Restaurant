8e858d7061bbf359c8318c0ef5a56a0f31639227
francothegenius <A01634495@itesm.mx>
2020-04-13 16:17:26 -0500

    formato desc

--- app/src/main/java/com/example/assignment03_menu_restaurant/Description_activity.java
+++ app/src/main/java/com/example/assignment03_menu_restaurant/Description_activity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Description_activity extends AppCompatActivity {
@@ -25,6 +26,28 @@ public class Description_activity extends AppCompatActivity {
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
         int dish = bundle.getInt("dish");
+        ImageView imagen = findViewById(R.id.imageDes);
+
+        switch(dish){
+            case 1:
+                imagen.setImageResource(R.drawable.quesadillas);
+                break;
+            case 2:
+                imagen.setImageResource(R.drawable.tamal);
+                break;
+            case 3:
+                imagen.setImageResource(R.drawable.birria);
+                break;
+            case 4:
+                imagen.setImageResource(R.drawable.hamburguesa);
+                break;
+            case 5:
+                imagen.setImageResource(R.drawable.sandwich);
+                break;
+            case 6:
+                imagen.setImageResource(R.drawable.tacos);
+                break;
+        }
 
         Cursor data = db.getData(dish);
 
